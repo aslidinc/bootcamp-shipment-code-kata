ac44c11ebc24b09c41accb76e1ce0d7ad705bfd3
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2023-09-10 17:11:24 +0300

    Code refactor

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -8,60 +8,60 @@ public class Basket {
 
     public ShipmentSize getShipmentSize() {
 
-        if ( products.isEmpty() ) { return null; }
+        if (products.isEmpty()) { return null; }
 
-        int[] sizeInstances = countSizeInstances();
+        int[] sizeQuantities = countSizeQuantities();
 
-        if ( products.size() >= 3  ) {
-            for ( int i = 0 ; i < sizeInstances.length ; i++ ) {
-                if ( sizeInstances[ i ] >= 3 ) {
-                    return productValueToSize( i + 1 );
+        if (products.size() >= 3) {
+            for (int i = 0 ; i < sizeQuantities.length ; i++) {
+                if (sizeQuantities[ i ] >= 3) {
+                    return shipmentNumericValueToSize(i + 1);
                 }
             }
         }
-        return productValueToSize( findMaxSizedProduct() );
+        return findMaxSized(sizeQuantities);
     }
 
-    public int[] countSizeInstances() {
-        int[] sizeInstances = {0, 0, 0, 0};
+    private int[] countSizeQuantities () {
+        int[] sizeQuantities = new int[ShipmentSize.values().length];
 
-        for ( int i = 0; i < products.size(); i++ ) {
-            sizeInstances[productSizeToValue(products.get( i ).getSize())] =
-                    sizeInstances[productSizeToValue(products.get( i ).getSize())] + 1;
+        for (int i = 0; i < products.size(); i++) {
+            int shipmentValue = shipmentSizeToNumericValue(products.get(i).getSize());
+            sizeQuantities[shipmentValue]++;
         }
-        return sizeInstances;
+        return sizeQuantities;
     }
 
-    public int findMaxSizedProduct() {
-        int maxSize = 0;
+    private ShipmentSize findMaxSized (int[] sizeQuantities) {
+        int maxShipmentNumericValue = 0;
 
-        for ( int i = 0; i < products.size(); i++ ) {
-            int currentProductSizeValue = productSizeToValue( getProducts().get( i ).getSize() );
-            if(currentProductSizeValue > maxSize) {
-                maxSize = currentProductSizeValue;
+        for (int i = sizeQuantities.length - 1 ; i >= 0; i--) {
+            if(sizeQuantities[i] > 0) {
+                maxShipmentNumericValue = i;
+                break;
             }
         }
-        return maxSize;
+        return shipmentNumericValueToSize(maxShipmentNumericValue);
     }
 
-    public int productSizeToValue(ShipmentSize shipmentSize) {
-        if ( shipmentSize == ShipmentSize.SMALL ) {
+    private int shipmentSizeToNumericValue (ShipmentSize shipmentSize) {
+        if (shipmentSize == ShipmentSize.SMALL) {
             return 0;
-        } else if ( shipmentSize == ShipmentSize.MEDIUM ) {
+        } else if (shipmentSize == ShipmentSize.MEDIUM) {
             return 1;
-        } else if ( shipmentSize == ShipmentSize.LARGE ) {
+        } else if (shipmentSize == ShipmentSize.LARGE) {
             return 2;
         } else {
             return 3;
         }
     }
 
-    public ShipmentSize productValueToSize(int productValue) {
-        if ( productValue == 0 ) {
+    private ShipmentSize shipmentNumericValueToSize (int shipmentNumericValue) {
+        if (shipmentNumericValue == 0) {
             return ShipmentSize.SMALL;
-        } else if ( productValue == 1 ) {
+        } else if (shipmentNumericValue == 1) {
             return ShipmentSize.MEDIUM ;
-        } else if ( productValue == 2 ) {
+        } else if (shipmentNumericValue == 2) {
             return ShipmentSize.LARGE;
         } else {
             return ShipmentSize.X_LARGE;
