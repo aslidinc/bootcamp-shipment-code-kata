b212e95852a6e19cff2db9074d6244509c27ca68
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2023-09-10 12:31:07 +0300

    Implementation of the logic
    Refactor will be done

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -7,7 +7,65 @@ public class Basket {
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+
+        if ( products.isEmpty() ) { return null; }
+
+        int[] sizeInstances = countSizeInstances();
+
+        if ( products.size() >= 3  ) {
+            for ( int i = 0 ; i < sizeInstances.length ; i++ ) {
+                if ( sizeInstances[ i ] >= 3 ) {
+                    return productValueToSize( i + 1 );
+                }
+            }
+        }
+        return productValueToSize( findMaxSizedProduct() );
+    }
+
+    public int[] countSizeInstances() {
+        int[] sizeInstances = {0, 0, 0, 0};
+
+        for ( int i = 0; i < products.size(); i++ ) {
+            sizeInstances[productSizeToValue(products.get( i ).getSize())] =
+                    sizeInstances[productSizeToValue(products.get( i ).getSize())] + 1;
+        }
+        return sizeInstances;
+    }
+
+    public int findMaxSizedProduct() {
+        int maxSize = 0;
+
+        for ( int i = 0; i < products.size(); i++ ) {
+            int currentProductSizeValue = productSizeToValue( getProducts().get( i ).getSize() );
+            if(currentProductSizeValue > maxSize) {
+                maxSize = currentProductSizeValue;
+            }
+        }
+        return maxSize;
+    }
+
+    public int productSizeToValue(ShipmentSize shipmentSize) {
+        if ( shipmentSize == ShipmentSize.SMALL ) {
+            return 0;
+        } else if ( shipmentSize == ShipmentSize.MEDIUM ) {
+            return 1;
+        } else if ( shipmentSize == ShipmentSize.LARGE ) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    public ShipmentSize productValueToSize(int productValue) {
+        if ( productValue == 0 ) {
+            return ShipmentSize.SMALL;
+        } else if ( productValue == 1 ) {
+            return ShipmentSize.MEDIUM ;
+        } else if ( productValue == 2 ) {
+            return ShipmentSize.LARGE;
+        } else {
+            return ShipmentSize.X_LARGE;
+        }
     }
 
     public List<Product> getProducts() {
